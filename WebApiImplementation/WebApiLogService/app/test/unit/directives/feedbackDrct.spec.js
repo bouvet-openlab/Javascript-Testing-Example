826b92700fae9d9caca84c120e47589d4d343ace
Eirik Bell <eirik.bell@bouvet.no>
2014-09-22 13:32:15 +0200

    Directive tests now passing

---
+++ WebApiImplementation/WebApiLogService/app/test/unit/directives/feedbackDrct.spec.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+describe('feedback directive', function() {
+	var el;
+	var feedbackText;
+
+    beforeEach(module('app'));
+
+    beforeEach(inject(function($compile, $rootScope) {
+    	feedbackText = '1 + 1 = 2';
+    	var scope = $rootScope;
+    	scope.feedback = feedbackText;
+
+    	el = angular.element('<feedback feedback="{{feedback}}" />');
+    	$compile(el)(scope);
+        scope.$digest();
+    }));
+
+    it('binds feedback', function () {
+
+        expect(el.text()).toContain(feedbackText);
+    });
+});
---
+++ WebApiImplementation/WebApiLogService/app/test/unit/directives/feedbackTemplateDrct.Spec.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+describe('feedbackTemplate directive', function () {
+    var el;
+    var feedbackText;
+
+    beforeEach(module('app'));
+
+    beforeEach(module('app/src/directives/feedbackTemplate.html'));
+
+    beforeEach(inject(function ($compile, $rootScope) {
+        feedbackText = '1 + 1 = 2';
+        var scope = $rootScope;
+        scope.feedback = feedbackText;
+
+        el = angular.element('<feedback-template feedback="{{feedback}}" />');
+        $compile(el)(scope);
+        scope.$digest();
+        //console.log(el[0].outerHTML);
+    }));
+
+    it('binds feedback', function () {
+
+        expect(el.text()).toContain(feedbackText);
+    });
+});
